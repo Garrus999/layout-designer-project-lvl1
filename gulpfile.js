1a7d18a68d22ecf1d025db9c0015f1e1c475e27b
garrus999 <novikoveg136@gmail.com>
2021-02-21 15:29:53 +0300

    add

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 function style () {
-    return gulp.src('./src/scss/**/*.scss')
+    return gulp.src('./scss/**/*.scss')
                 .pipe(sass())
                 .pipe(gulp.dest('./css'))
                 .pipe(browserSync.stream())
@@ -15,8 +15,8 @@ function watch () {
             baseDir: './'
         }
     })
-    gulp.watch('./src/scss/**/*.scss', style)
-    gulp.watch('./src/*.html').on('change', browserSync.reload);
+    gulp.watch('./scss/**/*.scss', style)
+    gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
 exports.style = style;
